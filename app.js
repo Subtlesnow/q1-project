3c85bf0754f892707a169846e02ef6748e67432c
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-11 17:32:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   // console.log('hello hello');
 
   $.get(" https://thecountedapi.com/api/counted", function(data) {
-    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].city);
+
+    }
   })
 
-  // $('form').on('submit', function(event) {
-  //   event.prevent
-  // })
+  
+
+
 })
