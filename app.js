78b7a3f7180b284452be309c9aa2546335fe73a0
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-14 15:08:44 -0700

    added search buttons

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// google geocode key AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk
+
 $(document).ready(function() {
-  // console.log('hello hello');
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+
+    var city = $('#city');
+    var state = $('#state');
+    console.log("city", "state")
+    getPlace()
+  })
 
   // $.get(" https://thecountedapi.com/api/counted", function(data) {
   //   for (let i = 0; i < data.length; i++) {
@@ -9,6 +19,12 @@ $(document).ready(function() {
   // })
 
 
+}) //End of page load
 
 
-})
+function getPlace(city, state) {
+  $.get("https://maps.googleapis.com/maps/api/geocode/json?city=" + city + "state=" + state + "&key=AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk", function(place) {
+    console.log("i got it");
+    console.log(place);
+  })
+}
