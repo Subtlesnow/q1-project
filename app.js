7a0ea815174dc415d3c6eefd49ee1cd5855a78cd
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-14 15:54:55 -0700

    google lat/long request

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ $(document).ready(function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
 
-    var city = $('#city');
-    var state = $('#state');
-    console.log("city", "state")
-    getPlace()
+    var city = $('#city').val();
+    var state = $('#state').val();
+    console.log(city, state)
+
+      // Google city/state latitude/longitude request
+      $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+city+"?state="+state+"&key=AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk", function(place) {
+        console.log("i got it");
+        console.log(place);
+      })
+
   })
+
 
   // $.get(" https://thecountedapi.com/api/counted", function(data) {
   //   for (let i = 0; i < data.length; i++) {
@@ -20,11 +27,3 @@ $(document).ready(function() {
 
 
 }) //End of page load
-
-
-function getPlace(city, state) {
-  $.get("https://maps.googleapis.com/maps/api/geocode/json?city=" + city + "state=" + state + "&key=AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk", function(place) {
-    console.log("i got it");
-    console.log(place);
-  })
-}
