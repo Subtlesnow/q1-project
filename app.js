fa655a91d458a6b3b74e926388b10abb8de8b403
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-14 14:32:05 -0700

    add city state form

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
+// google geocode key AIzaSyB3B_9IQ1q64ipGFN0nMo0sGOMg0-jjtoU
+
+
 $(document).ready(function() {
   // console.log('hello hello');
 
-  $.get(" https://thecountedapi.com/api/counted", function(data) {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].age);
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var city = $('#city');
+    console.log("city")
+    var state = $('#state');
+    console.log("state");
+    console.log("hello");
+    // var $input = $('input[name=search]');
+    // var place = $input.val('');
+    // console.log(place);
 
-    }
   })
 
+  // $.get(" https://thecountedapi.com/api/counted", function(data) {
+  //   for (let i = 0; i < data.length; i++) {
+  //     console.log(data[i].age);
+  //
+  //   }
+  // })
+
+  // get geocode of city and state
+  // function getLocation(place) {
+  //   $.get("https://maps.googleapis.com/maps/api/geocode/json?city=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB3B_9IQ1q64ipGFN0nMo0sGOMg0-jjtoU",
+  // function(place ))
+  // }
 
 
 
