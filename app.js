392297c63ac90762f64c5aa83700478441d01828
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-14 14:37:45 -0700

    started new direction

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   // console.log('hello hello');
 
-  $.get(" https://thecountedapi.com/api/counted", function(data) {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].age);
-
-    }
-  })
+  // $.get(" https://thecountedapi.com/api/counted", function(data) {
+  //   for (let i = 0; i < data.length; i++) {
+  //     console.log(data[i].age);
+  //
+  //   }
+  // })
 
 
 
