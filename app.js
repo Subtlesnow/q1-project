f46523af7b46cc938d752f3c823a956b2ae50f47
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-15 15:45:44 -0700

    map not loading

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // google geocode key AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk
 
-// mapboxgl.accessToken = 'pk.eyJ1Ijoic3VidGxlcmlkZXNub3ciLCJhIjoiY2l3a2F2cnVuMDAzNDJvcDI2Nnc3d3R3MiJ9.2GBe5fY1JXBCg48VcATbZw';
-// var map = new mapboxgl.Map({
-//     container: 'map', // container id
-//     style: 'mapbox://styles/mapbox/dark-v9', //hosted style id
-//     center: [-98, 39], // starting position
-//     zoom: 3, // starting zoom
-//     "pitch": 50 //pitch
-// });
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3VidGxlcmlkZXNub3ciLCJhIjoiY2l3a2F2cnVuMDAzNDJvcDI2Nnc3d3R3MiJ9.2GBe5fY1JXBCg48VcATbZw';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/subtleridesnow/ciwn3f9w400452ppll27n9i56', //hosted style id
+    center: [-98, 39], // starting position
+    zoom: 3, // starting zoom
+    "pitch": 50 //pitch
+});
 
 
 $(document).ready(function() {
 
+
   $('form').on('submit', function(event) {
     event.preventDefault();
-
     var city = $('#city').val();
     // console.log(city)
     $('#city').val('');
@@ -25,7 +25,6 @@ $(document).ready(function() {
 
       // Google city/state latitude/longitude request
       $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+city+"?state="+state+"&key=AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk", function(place) {
-        // console.log("i got it");
         // console.log(place)
         var placeLat = place.results[0].geometry.location.lat;
         // console.log(placeLat);
@@ -53,7 +52,7 @@ $(document).ready(function() {
         // console.log(addressList)
 
 
-        // // Google address coords request
+        // Google address coords request
         addressList.forEach(function(addressStr) {
           $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+addressStr+"+"+city+"+%22?state=%22+"+state+"+%22&key=AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk",
             function(house) {
@@ -62,19 +61,18 @@ $(document).ready(function() {
               console.log(addressLat);
               var addressLng = house.results[0].geometry.location.lng;
               console.log(addressLng);
-            })
+
+          }) //End of get request
         }) // End of Google address coords
-
-
-
-
 
 
 
     }) // End of Counted API request
 
 
+
   }) //End of form submit
+
 
 
 }) //End of page load
