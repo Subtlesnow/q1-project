cd6f743373cf65d522d2db49cf6228a5bec0e391
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-14 17:05:23 -0700

    counted API request complete

--- app.js
+++ app.js
@@ -6,24 +6,34 @@ $(document).ready(function() {
     event.preventDefault();
 
     var city = $('#city').val();
+    // clear field function
     var state = $('#state').val();
+    // clear field function
     console.log(city, state)
+
 
       // Google city/state latitude/longitude request
       $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+city+"?state="+state+"&key=AIzaSyB_btmf41ZGYvX0ru8FBtb9gOgVWBOp5Lk", function(place) {
         console.log("i got it");
-        console.log(place);
+        console.log(place)
+        var placeLat = place.results[0].geometry.location.lat;
+        console.log(placeLat);
+        var placeLng = place.results[0].geometry.location.lng;
+        console.log(placeLng);
       })
 
-  })
+      // Counted API request
+      var countedAPI = "https://thecountedapi.com/api/counted?state="+state+"&city="+city
+      // console.log("label",countedAPI)
+      $.get(countedAPI, function(data) {
+        for(let i = 0; i < data.length; i++) {
+          console.log(data[i].address);
+        }
+    })
 
 
-  // $.get(" https://thecountedapi.com/api/counted", function(data) {
-  //   for (let i = 0; i < data.length; i++) {
-  //     console.log(data[i].age);
-  //
-  //   }
-  // })
+  }) //End of form submit
+
 
 
 }) //End of page load
